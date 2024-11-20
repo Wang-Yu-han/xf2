732bebc8f156a5dadfa9e5a8bb71ff3306e0aee5
Wang Yuhan <20246779@stu.neu.edu.cn>
2024-11-20 17:33:51 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,20 @@
+import rospy
+from std_msgs.msg import String
+def odometry_callback(data):
+    rospy.loginfo("Received Odometry message: %s",data)
+def imu_callback(data):
+    rospy.loginfo("Receive Imu message: %s",data)
+def laserscan_callback(data):
+    rospy.loginfo("Received LaserScan message: %s", data)
+def magfield_callback(data):
+    rospy.loginfo("Received MagneticField message: %s", data)
+def listener():
+    rospy.init_node('listener_node', anonymous=True)
+    rospy.Subscriber("/driver/encoder", Odometry, odometry_callback)
+    rospy.Subscriber("/driver/eul", Imu, imu_callback)
+    rospy.Subscriber("/driver/imu", Imu, imu_callback)
+    rospy.Subscriber("/driver/scan", LaserScan, laserscan_callback)
+    rospy.Subscriber("/driver/mag", MagneticField, magfield_callback)
+    rospy.spin()
+if __name__=='__main__':
+    listener()
